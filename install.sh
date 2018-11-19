e177827affaa09a7ce966a682ab543484578c68d
Jeff "Huey" Huelsbeck <huey@carpetelam.com>
2018-11-19 12:56:22 +0000

    Testing install script for ubuntu

--- install.sh
+++ install.sh
@@ -3,19 +3,16 @@
 {
 
   # initialize node & yarn repos
-  curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
-  curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
-  sudo rpm --import https://dl.yarnpkg.com/rpm/pubkey.gpg
+  curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+  echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
-  # yum update & install
-  sudo yum update -y
-  sudo yum install -y automake fuse gcc-c++ git ncurses-devel nodejs openssl-devel protobuf-devel yarn zlib-devel zsh
-
-  # pip update
-  sudo pip install --upgrade pip
+  # apt update & install
+  sudo apt update -y
+  sudo apt install -y automake curl fuse gcc-c++ git ncurses-devel openssl-devel protobuf-devel wget zlib-devel zsh
+  sudo apt install --no-install-recommends yarn
 
   # enable zsh & clean up bash
-  sudo chsh -s /bin/zsh $(whoami)
+  sudo chsh -s /bin/zsh
   rm -rf .bash*
 
   # create support directories
