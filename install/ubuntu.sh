6939c09b50d790d9d8acb5e6a470e6ce5e769022
Jeff "Huey" Huelsbeck <huey@carpetelam.com>
2019-05-06 01:02:10 -0400

    Update ubuntu.sh

--- install/ubuntu.sh
+++ install/ubuntu.sh
@@ -2,10 +2,6 @@
 
 {
 
-  # upgrade apt-get
-  sudo apt-get update
-  sudo apt-get upgrade -y
-
   # install yarn
   curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
   echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
