82e76f1b17a4e7cd225f64d53ef20a057f4444fd
Jeff "Huey" Huelsbeck <huey@carpetelam.com>
2019-05-09 15:25:07 +0000

    Add pass to docker install

--- install/ubuntu.sh
+++ install/ubuntu.sh
@@ -19,7 +19,7 @@
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
   sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
   sudo apt-get update
-  sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+  sudo apt-get install -y docker-ce docker-ce-cli containerd.io pass
   sudo usermod -a -G docker $USER
   sudo curl -L https://github.com/docker/compose/releases/download/1.24.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
   sudo chmod +x /usr/local/bin/docker-compose
