8ec88377a8d76e876be3d993f22dc8d8a3bf5b85
Jeff "Huey" Huelsbeck <huey@carpetelam.com>
2019-05-09 03:12:44 +0000

    Add docker-compose install

--- install/ubuntu.sh
+++ install/ubuntu.sh
@@ -21,6 +21,8 @@
   sudo apt-get update
   sudo apt-get install -y docker-ce docker-ce-cli containerd.io
   sudo usermod -a -G docker $USER
+  sudo curl -L https://github.com/docker/compose/releases/download/1.24.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
 
   # install neovim
   sudo add-apt-repository ppa:neovim-ppa/stable
