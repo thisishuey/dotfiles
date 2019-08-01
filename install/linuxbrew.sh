71e21d0d93388d6c2c3a10c6f8bfced4210555ea
Jeff "Huey" Huelsbeck <huey@carpetelam.com>
2019-08-01 03:47:54 +0000

    Add asdf and fish dotfiles

--- install/linuxbrew.sh
+++ install/linuxbrew.sh
@@ -50,8 +50,12 @@
 
   # setup dotfiles
   git clone https://github.com/thisishuey/dotfiles.git ~/.dotfiles
+  mkdir -p ~/.config/fish/functions
   mkdir -p ~/.config/nvim
   mkdir -p ~/.vim/after/syntax
+  ln -s ~/.dotfiles/asdf/tool-versions.symlink ~/.tool-versions
+  ln -s ~/.dotfiles/fish/config.fish.symlink ~/.config/fish/config.fish
+  ln -s ~/.dotfiles/fish/fish_mode_prompt.fish.symlink ~/.config/fish/functions/fish_mode_prompt.fish
   ln -s ~/.dotfiles/git/gitconfig.symlink ~/.gitconfig
   ln -s ~/.dotfiles/tmux/tmux.conf.symlink ~/.tmux.conf
   ln -s ~/.dotfiles/vim/editorconfig.symlink ~/.editorconfig
