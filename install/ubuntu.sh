b77b82b4da136e4e6e4d9b283f76fbe2b225da01
Jeff "Huey" Huelsbeck <huey@carpetelam.com>
2020-06-22 20:14:59 -0400

    Update ubuntu.sh

--- install/ubuntu.sh
+++ install/ubuntu.sh
@@ -2,115 +2,97 @@
 
 {
 
-  # install yarn
-  curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-  echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-  sudo apt-get update
-  sudo apt-get install -y --no-install-recommends yarn
+  # pre install
+  # tmux new -A -s dotfiles-setup
+  # sudo apt update
+  # sudo apt upgrade
+  # sudo apt install fish gcc
+  # sudo chsh -s /home/linuxbrew/.linuxbrew/bin/fish $(whoami)
+  # rm -rf ~/.bash* ~/.profile
+  # sudo reboot
 
-  # install mosh
-  sudo add-apt-repository ppa:keithw/mosh
-  sudo apt-get update
-  sudo apt-get install -y mosh
+  # DO NOT NEED
+  # sudo apt install -y build-essential
+  # sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+  # eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+  # brew install fish gcc
+  # sudo chsh -s /home/linuxbrew/.linuxbrew/bin/fish $(whoami)
+  # rm -rf ~/.bash* ~/.profile
+  # sudo reboot
 
+  # install dev tools
+  tmux new -A -s dotfiles-setup
+  sudo apt install mosh neovim silversearcher-ag
+  sudo snap install hub --classic
+  git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.7.8
+  mkdir -p ~/.config/fish/completions
+  cp ~/.asdf/completions/asdf.fish ~/.config/fish/completions
+
+  # DO NOT NEED
+  # eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+  # brew install asdf hub mosh neovim the_silver_searcher
+
+  # DO NOT NEED
+  # install rails dependencies
+  # brew install postgres redis
+
+  # DO NOT NEED
   # install docker
-  sudo apt-get update
-  sudo apt-get install apt-transport-https gnupg-agent
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-  sudo apt-get update
-  sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-  sudo usermod -a -G docker $USER
+  # brew install docker docker-compose docker-machine
 
-  # install docker-credential-pass
-  sudo apt-get update
-  sudo apt-get install -y pass
-  curl -L https://github.com/docker/docker-credential-helpers/releases/download/v0.6.2/docker-credential-pass-v0.6.2-amd64.tar.gz | sudo tar -xz -C /usr/local/bin/
-  sudo chmod +x /usr/local/bin/docker-credential-pass
-
-  # install docker-compose
-  sudo curl -L https://github.com/docker/compose/releases/download/1.24.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-  sudo chmod +x /usr/local/bin/docker-compose
-
-  # install docker-machine
-  base=https://github.com/docker/machine/releases/download/v0.16.0 &&
-  curl -L $base/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine &&
-  sudo install /tmp/docker-machine /usr/local/bin/docker-machine
-
-  # install neovim
-  sudo add-apt-repository ppa:neovim-ppa/stable
-  sudo apt-get update
-  sudo apt-get install -y neovim python-dev python-pip python3-dev python3-pip
-  sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
-  sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
-  sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
-
-  # install PHP
-  sudo apt-get update
-  sudo apt-get install -y php php-pear php-fpm php-dev php-zip php-curl php-xmlrpc php-gd php-mysql php-mbstring php-xml libapache2-mod-php unzip
-  sudo systemctl disable apache2
-
-  # install composer
-  php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-  php -r "if (hash_file('sha384', 'composer-setup.php') === '48e3236262b34d30969dca3c37281b3b4bbe3221bda826ac6a9a62d6444cdb0dcd0615698a5cbe587c3f0fe57a54d8f5') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-  php composer-setup.php
-  php -r "unlink('composer-setup.php');"
-  sudo mv composer.phar /usr/local/bin/composer
-  sudo chmod +x /usr/local/bin/composer
-
-  # install RoR
-  gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-  curl -sSL https://get.rvm.io | bash -s stable --ruby
-  gem install rails haml
-
-  # install zsh
-  sudo apt-get update
-  sudo apt-get install -y zsh silversearcher-ag
-  sudo chsh -s /bin/zsh $(whoami)
-  zsh
-  rm -rf ~/.bash*
-
-  # install nvm
-  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
-  sudo apt-get update
-  sudo apt-get install -y libcap2-bin
-  sudo setcap cap_net_bind_service=+ep `readlink -f \`which node\``
-
-  # install ctags
-  sudo apt-get update
-  sudo apt-get install -y pkg-config
-  git clone https://github.com/universal-ctags/ctags.git
-  cd ctags
-  ./autogen.sh
-  ./configure
-  make
-  sudo make install
-  cd ..
-  rm -rf ctags
+  # DO NOT NEED
+  # configure neovim as default editor
+  # sudo update-alternatives --install /usr/bin/vi vi /home/linuxbrew/.linuxbrew/bin/nvim 60
+  # sudo update-alternatives --install /usr/bin/vim vim /home/linuxbrew/.linuxbrew/bin/nvim 60
+  # sudo update-alternatives --install /usr/bin/editor editor /home/linuxbrew/.linuxbrew/bin/nvim 60
 
   # install base16-shell
-  git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
-  ln -s ~/.config/base16-shell/scripts/base16-default-dark.sh ~/.base16_theme
+  curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
+  fisher add tomyun/base16-fish
 
-  # install antigen
-  git clone https://github.com/zsh-users/antigen.git ~/.config/antigen
+  # DO NOT NEED
+  # install ctags
+  # sudo apt-get update
+  # sudo apt-get install -y pkg-config
+  # git clone https://github.com/universal-ctags/ctags.git
+  # cd ctags
+  # ./autogen.sh
+  # ./configure
+  # make
+  # sudo make install
+  # cd ..
+  # rm -rf ctags
+
+  # DO NOT NEED
+  # install docker-credential-pass
+  # sudo apt-get update
+  # sudo apt-get install -y pass
+  # curl -L https://github.com/docker/docker-credential-helpers/releases/download/v0.6.2/docker-credential-pass-v0.6.2-amd64.tar.gz | sudo tar -xz -C /usr/local/bin/
+  # sudo chmod +x /usr/local/bin/docker-credential-pass
 
   # ssh-keygen
-  instanceId=$(curl --silent http://169.254.169.254/latest/meta-data/instance-id)
-  ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -q -N "" -C "$instanceId"
+  ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -q -N "" -C (curl --silent http://169.254.169.254/latest/meta-data/instance-id)
 
   # setup dotfiles
   git clone https://github.com/thisishuey/dotfiles.git ~/.dotfiles
   mkdir -p ~/.config/nvim
-  mkdir -p ~/.vim/after/syntax
+  ln -s ~/.dotfiles/asdf/tool-versions.symlink ~/.tool-versions
+  ln -s ~/.dotfiles/fish/config.fish.symlink ~/.config/fish/config.fish
+  ln -s ~/.dotfiles/fish/fish_mode_prompt.fish.symlink ~/.config/fish/functions/fish_mode_prompt.fish
   ln -s ~/.dotfiles/git/gitconfig.symlink ~/.gitconfig
-  ln -s ~/.dotfiles/node/nvmrc.symlink ~/.nvmrc
   ln -s ~/.dotfiles/tmux/tmux.conf.symlink ~/.tmux.conf
   ln -s ~/.dotfiles/vim/editorconfig.symlink ~/.editorconfig
+  ln -s ~/.dotfiles/vim/init.vim.symlink ~/.config/nvim/init.vim
   ln -s ~/.dotfiles/vim/vimrc.symlink ~/.vimrc
   ln -s ~/.dotfiles/vim/vimrc_background.symlink ~/.vimrc_background
-  ln -s ~/.dotfiles/vim/init.vim.symlink ~/.config/nvim/init.vim
-  ln -s ~/.dotfiles/vim/xml.vim.symlink ~/.vim/after/syntax/xml.vim
-  ln -s ~/.dotfiles/zsh/zshrc.symlink ~/.zshrc
+  
+  # post install
+  # sudo reboot
+  # asdf plugin-add golang
+  # asdf plugin-add nodejs
+  # asdf plugin-add yarn
+  # sudo apt install -y zlib1g-dev
+  # bash /home/linuxbrew/.linuxbrew/opt/asdf/plugins/nodejs/bin/import-release-team-keyring
+  # asdf install
 
 }
